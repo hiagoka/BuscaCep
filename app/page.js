3e1b18029229c2922916093cefc3029c9001899b
Hiago Kalil <125253798+hiagoka@users.noreply.github.com>
2024-03-08 11:15:22 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,48 @@
+'use client'
+
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useForm} from 'react-hook-form';
+import {useRouter} from 'next/navigation';
+import {cepFormSchema} from './components/zod';
+
+
+export default function Home() {
+
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    resolver: zodResolver(cepFormSchema)
+  })
+
+  const router = useRouter()
+
+  function onSubmit(data){
+    console.log(data)
+    router.push(`/cep/${data.cep}`)
+  }
+
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#1f2937]">
+
+      <div>
+        <h1 className="text-white text-center font-bold text-2x1 py-4"> Buscador de CEP</h1>
+
+        <form className="flex flex-col gap-y-8 items-center"
+        onSubmit={handleSubmit(onSubmit)}>
+          <input type="text"  className=" text-black w=1/2 p-2 rounded-lg text-center font-bold text-lg"{...register("cep")} /> 
+          {errors.cep &&(
+          <span>
+            {errors.cep.message}
+          </span>
+          )}
+          <button type="submit" className="bg-blue-700 px-8 py-3 rounded-lg font-bold text-white 
+          text-lg hover:bg-blue-800 transition-all duration-300 ease-in-out">Buscar </button>
+        </form>
+      </div>
+      
+    </main>
+  );
+}
